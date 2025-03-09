75d0c1037b467fead9088544f1bc832b89fbf8b2
Vinayak-cloud-debug <vinayakrodd56@gmail.com>
2025-03-09 20:27:07 +0530

    Update ResetPassword.jsx

--- src/pages/resetPassword/ResetPassword.jsx
+++ src/pages/resetPassword/ResetPassword.jsx
@@ -11,7 +11,7 @@ const ResetPassword = () => {
   const handleResetPassword = async () => {
     try {
       const email = localStorage.getItem("email");
-      const response = await fetch("http://localhost:5000/api/auth/reset-password", {
+      const response = await fetch("https://dsavisualizationbackend-tmii.onrender.com/api/auth/reset-password", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
