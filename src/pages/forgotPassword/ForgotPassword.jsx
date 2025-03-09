bbbe36f3357e1d6f8107100dbf70e8df84f955d6
Vinayak-cloud-debug <vinayakrodd56@gmail.com>
2025-03-09 20:26:54 +0530

    Update ForgotPassword.jsx

--- src/pages/forgotPassword/ForgotPassword.jsx
+++ src/pages/forgotPassword/ForgotPassword.jsx
@@ -14,7 +14,7 @@ const ForgotPassword = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/forgot-password", {
+      const response = await fetch("https://dsavisualizationbackend-tmii.onrender.com/api/auth/forgot-password", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email }),
